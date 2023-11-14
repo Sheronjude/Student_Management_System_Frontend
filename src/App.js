fa0cbd0bb770bec2d40ed14d96d6a71386b1f928
Anto Sheron <sheronjude8@gmail.com>
2023-11-14 19:32:14 +0530

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
 
-import './App.css';
-import Home from './Home';
-import StudentsView from './component/student/StudentsView';
+import "./App.css";
+import Home from "./Home";
+import StudentsView from "./component/student/StudentsView";
+import { Route, Router, Routes } from "react-router-dom";
+import NavBar from "./component/common/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h2>hello</h2>
-      <Home/>
-      <StudentsView/>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/student-details" element={<StudentsView />}></Route>
+      </Routes>
     </div>
   );
 }
